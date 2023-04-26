0c08924dff97103745bd8aeb766cf744b75a51f4
Olasunkanmi <olasunkanmiibalogun@gmail.com>
2023-04-26 05:47:01 +0100

    implement authentication and protected routes with Firebase

--- src/Login-Auth.tsx
+++ src/Login-Auth.tsx
@@ -2,12 +2,54 @@ import hero from './assets/loginHero.png';
 import logo from './assets/logo.png';
 import { useState } from 'react'
 import Input from './components/login-auth/Input';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {  useNavigate } from 'react-router-dom'
+import { auth } from './components/login-auth/firebaseConfig';
 
 export default function LoginAuth() {
-    
+  
+  const navigate = useNavigate()
   const [ email, setEmail ] = useState<string>('');
   const [ password, setPassword ] = useState<string>('')
   const [ hide, setHide ] = useState<boolean>(true)
+  const [ helperText, setHelperText ] = useState<string>('')
+  const [ signUp, setSignUp ] = useState<boolean>(true)
+  const [ error, setError ] = useState<boolean>(false)
+
+  const handleSignUp = (e: React.FormEvent<HTMLButtonElement> ) => {
+    e.preventDefault()
+    createUserWithEmailAndPassword(auth, email, password)
+     .then(() => {
+      setHelperText('Congrats!, you can now Sign In')
+      setTimeout(() => setHelperText(''), 2000)
+      setSignUp(!signUp)
+      setError(false)
+      // const userId = auth.currentUser?.uid
+     }) 
+   .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    setError(true)
+    console.log(errorCode, errorMessage)
+   });
+ }
+
+  const handleSignIn = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      setError(false)
+      navigate('/dashboard/users')
+     })
+   .catch((error) => {
+   const errorCode = error.code;
+   (errorCode === 'auth/user-not-found!') ?
+    setHelperText('Email is not registered!') :
+    setHelperText('Invalid password')
+    setError(true)
+    setTimeout(() => setHelperText(''), 2000)
+  });
+}
 
     return (
         <div className='login-container'>
@@ -18,19 +60,21 @@ export default function LoginAuth() {
           <form className='login-form'>
             <div>
             <h1>Welcome!</h1>
-            <p>Enter details to login.</p>   
+            <p>Enter details to {signUp ? "login" : 'sign up'}.</p>   
             <div>
+            <p className={`${error ? 'error' : 'success'}`}>{helperText}</p>
               <div className='input-container'>
-              <Input type='text' placeholder={'Email'} value={email} onChange={setEmail}/>
+              <Input type='email' placeholder={'Email'} value={email} onChange={setEmail}/>
               </div>
               <div className='input-container'>
               <Input type={hide ? "password" : 'text'} placeholder={'Password'} value={password} onChange={setPassword}/>
-              <span onClick={() => setHide(!hide)}>{hide ? "Show": "Hide"}</span>
+              <span  className='show' onClick={() => setHide(!hide)}>{hide ? "Show": "Hide"}</span>
               </div>
             </div>
               <p>Forgot Password?</p>
-               <button>
-                Log In
+              <p>Don't have an account? <span onClick={() => setSignUp(!signUp)}>Sign Up</span></p>
+               <button type='submit' onClick={!signUp ? handleSignUp : handleSignIn}>
+                  {signUp ? ' Log In' : ' Sign Up'} 
                </button>
             </div>
           </form>
--- src/components/dashboard/Page.tsx
+++ src/components/dashboard/Page.tsx
@@ -1,7 +1,8 @@
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import { useState } from 'react';
-
+import { auth } from "../login-auth/firebaseConfig";
+import { Navigate } from "react-router-dom";
 type PageProps = {
    children: React.ReactNode,
    title?: string,
@@ -9,9 +10,15 @@ type PageProps = {
 
 export default function Page ({children, title}: PageProps) {
     
-    const [ show, setShow ] = useState<boolean>(false)
-
+    const [ show, setShow ] = useState<boolean>(false);
+    
     return (
+        <>
+        {
+        
+        (auth.currentUser === null) ? 
+        <Navigate to='/' replace={true}/> :
+        (
         <>
         <Navbar toggleSidebar={{ show, setShow}}/>
         <Sidebar show={ show}/>
@@ -19,6 +26,9 @@ export default function Page ({children, title}: PageProps) {
         { title ? <h1>{title}</h1> : '' }  {/* this will be useful for other not yet existent pages  */}
             {children}
         </div>
+        </>
+        )
+        }
         </>
     )
 }
